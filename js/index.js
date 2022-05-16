1304cc5979b9a8f0412a7812e3da7fc8fc1b4749
Yrla Hackzell <yrla.hackzell@hotmail.com>
2022-05-16 17:44:26 +0200

    travelsuggestions

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const buttonElem = document.querySelector(`#position-button`);
 const nearMe = document.querySelector(`#near-me-container`);
+
 var mykey = config.MY_KEY;
+
+
 
 buttonElem.addEventListener(`click`, () => {
   if (navigator.geolocation) {
@@ -66,49 +69,135 @@ function oneStop(timeObj) {
 
 /// söka själv
 
+const inputTo = document.querySelector(`#travel-to`)
+const suggestionsContainerFrom = document.querySelector(`#travel-suggestions-from`)
+const suggestionsContainerTo = document.querySelector(`#travel-suggestions-to`)
+const inputFrom = document.querySelector('#travel-from');
+const log = document.getElementById('values');
+inputFrom.addEventListener('input', updateValueFrom);
+inputTo.addEventListener('input', updateValueTo);
+let testarrayto =[]
+let testarrayfrom =[]
 
-const buttontest = document.querySelector(`#buttontest`);
-const hi = document.querySelector(`#hi`);
-const inputElemFrom = document.querySelector(`#travel-from`)
-const inputElemto = document.querySelector(`#travel-to`)
-
-
-const inputBtn = (e) => {
-    const word = inputElemFrom.value;
-
-   testConsole(word)
-
+// log.textContent = e.target.value;
+function updateValueFrom(e) {
+ word = e.target.value;
+  valueFrom(word)
+}
+function updateValueTo(e) {
+  word = e.target.value;
+   valueTo(word)
  }
-async function testConsole(word) {
+ 
+async function valueTo(word) {
+  const response = await fetch(
+    `https://api.resrobot.se/v2.1/location.name?input=${word}&format=json&accessId=${mykey}`
+  );
+  const data = await response.json();
+  suggestionArray = data.stopLocationOrCoordLocation
+ 
+travelSuggestionTo(suggestionArray)
+
+}
+async function valueFrom(word) {
     const response = await fetch(
       `https://api.resrobot.se/v2.1/location.name?input=${word}&format=json&accessId=${mykey}`
     );
     const data = await response.json();
-    console.log(data) 
+    suggestionArray = data.stopLocationOrCoordLocation
+   
+travelSuggestionFrom(suggestionArray)
+
+  }
+
+
+  function travelSuggestionFrom(suggestionArray) {
+    const suggObj = suggestionArray.map((item) => {
+      return { myname: item.StopLocation.name, id: item.StopLocation.extId};
+    });
+    renderSuggestionsFrom(suggObj)
+  
+  }
+  function travelSuggestionTo(suggestionArray) {
+    const suggObj = suggestionArray.map((item) => {
+      return { myname: item.StopLocation.name, id: item.StopLocation.extId};
+    });
+    renderSuggestionsTo(suggObj)
+  }
+
+
+  function renderSuggestionsTo(suggObj) {
+    const chooseSug = suggObj.map((item) => {
+      console.log(item)
+      return `<div id="nd"  onclick=tyBtnTo(${item.id})> ${item.myname} </div>`;
+    })
+    .join("");
+  suggestionsContainerTo.innerHTML = chooseSug;
+ 
+  }
+  function renderSuggestionsFrom(suggObj) {
+    const chooseSug = suggObj.map((item) => {
+      console.log(item)
+      return `<div id="nd"  onclick=tyBtnFrom(${item.id})> ${item.myname} </div>`;
+    })
+    .join("");
+  suggestionsContainerFrom.innerHTML = chooseSug;
+ 
   }
 
 
 
+  
 
+  async function tyBtnFrom(id) {
+    const response = await fetch(
+      `https://api.resrobot.se/v2.1/departureBoard?id=${id}&format=json&accessId=${mykey}`
+    );
+    const data = await response.json();
+   const mydag = data.Departure[1].stop
+   const myid = data.Departure[1].stopExtId
+extractId(myid)
+testarrayfrom.push(myid)
+   setinputFrom(mydag)
+  }
+  async function tyBtnTo(id) {
+    const response = await fetch(
+      `https://api.resrobot.se/v2.1/departureBoard?id=${id}&format=json&accessId=${mykey}`
+    );
+    const data = await response.json();
+   const mydag = data.Departure[1].stop
+   const myotherid = data.Departure[1].stopExtId
+   testarrayto.push(myotherid)
+extractId(myotherid)
+   setinputTo(mydag)
+  }
 
+function setinputFrom(mydag) {
+  console.log(mydag)
+  inputFrom.value = mydag
 
-buttontest.addEventListener(`click`, (event) => {
-  test();
-});
+}
+function setinputTo(mydag) {
+  console.log(mydag)
+  inputTo.value = mydag
 
-async function test() {
-  const response = await fetch(
-    `https://api.resrobot.se/v2.1/trip?format=json&originId=740000001&destId=740000003&passlist=true&showPassingPoints=true&accessId=${mykey}`
+}
+
+function extractId(myids) {
+ console.log(testarrayfrom)
+console.log(testarrayto)
+}
+
+ function inputBtn(params) {
+  let idTo = testarrayto[testarrayto.length - 1];
+  let idFrom = testarrayfrom[testarrayfrom.length - 1];
+ console.log(idFrom + idTo)
+loelo(idTo, idFrom)
+}
+async function loelo(idTo, idFrom) {
+   const response = await fetch(
+    `https://api.resrobot.se/v2.1/trip?format=json&originId=${idFrom}&destId=${idTo}&passlist=true&showPassingPoints=true&accessId=${mykey}`
   );
   const data = await response.json();
-  const trip = data.Trip;
-  destElem(trip);
-}
-function destElem(trip) {
-  const destObj = trip.map((item) => {
-    return { destination: item.Destination.name, origin: item.Origin.name };
-  });
-  console.log(destObj);
-
-  hi.innerHTML += `<li> ${destObj[2].destination + destObj[2].origin}  </li>`;
+  console.log(data)
 }
